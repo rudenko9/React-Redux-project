25230bdd45eb73c14dababc105f9462b662bb39f
github username <github email address>
2019-12-08 16:45:28 -0500

    added containers

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-    <h1>Events</h1>
+    <Header />
+    <Footer />
     </div>
   );
 }
---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return(
+
+    <footer>
+    <h6> {new Date().getFullYear()}  Have a nice trip.</h6>
+    </footer>
+  )
+};
+export default Footer;
