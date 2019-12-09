0390e247b4b9f7147a2b10a2c730c164e4bdf639
github username <github email address>
2019-12-08 21:52:22 -0500

    added fetch

--- client/src/containers/CreateEvents/index.js
+++ client/src/containers/CreateEvents/index.js
@@ -1,23 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
+
 import Button from '@material-ui/core/Button'
 import Typography from '@material-ui/core/Typography'
 import TextField from '@material-ui/core/TextField';
 
 const CreateEvents = () => {
+
+  const [state, setState] = useState({
+    name: '',
+    address: '',
+    date_time: '',
+    price: '',
+    image: '',
+    phone_number: ''
+  });
+
+
+  const handleOnChange = (label) => (event) => {
+    const value = event.target.value
+    if (value.length > 35) return;
+    setState(preState => ({...preState, [label]: value}))
+  }
+
+const handleSave = () => {
+
+const data = {
+...state,
+
+
+
+}
+
+  const options = {
+         method: "POST",
+         body: JSON.stringify(state),
+         credentials: 'omit',
+         //setting the headers
+         headers:{
+         'Content-Type': 'application/json', Accept: "application/json"
+        },
+       };
+
+       fetch(`http://localhost:3000/api/events/create`, options)
+    .then(res => {
+      console.log( res);
+     }
+    )
+    .catch(console.error)
+};
+
   return(
     <main>
     <br/>
-      <Typography variant='h3' color='secondary'> You can create event here </Typography>
+      <Typography variant='h5' color='secondary'> You can create event here </Typography>
 
       <form noValidate autoComplete='off' className='App-create-form'>
-        <TextField label='Event name' variant='outlined' margin="normal" placeholder="Event name"/>
-        <TextField label='Address'  variant='outlined' margin="normal" placeholder="address"/>
-        <TextField label='Date_Time' variant='outlined' margin="normal" placeholder="Date & Time"/>
-        <TextField label='Price' variant='outlined' margin="normal" placeholder="Price"/>
-        <TextField label='Image' variant='outlined' margin="normal" placeholder="image url"/>
-        <TextField label='Phone_Number' variant='outlined' margin="normal" placeholder="Phone Number"/>
+        <TextField onChange={handleOnChange('name')} value={state.name}  label='Event name' variant='outlined' margin="normal" placeholder="Event name"/>
+        <TextField onChange={handleOnChange('address')} value={state.address} label='Address'  variant='outlined' margin="normal" placeholder="address"/>
+        <TextField onChange={handleOnChange('date_time')} value={state.date_time} label='Date_Time' variant='outlined' margin="normal" placeholder="Date & Time"/>
+        <TextField onChange={handleOnChange('price')} value={state.price} label='Price' variant='outlined' margin="normal" placeholder="Price"/>
+        <TextField onChange={handleOnChange('image')} value={state.image} label='Image' variant='outlined' margin="normal" placeholder="image url"/>
+        <TextField onChange={handleOnChange('phone_number')} value={state.phone_number} label='Phone_Number' variant='outlined' margin="normal" placeholder="Phone Number"/>
 
-      <Button color='default' variant='outlined'> Save your event</Button>
+      <Button color='default' variant='outlined' onClick={handleSave} > Save your event </Button>
 
        </form>
      </main>
